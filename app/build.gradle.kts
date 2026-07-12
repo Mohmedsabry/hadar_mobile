5747c166eccbbe5218c9b55153df5acbb49845ae
MohmedSabry <mayjak60@gmail.com>
2026-07-12 23:10:49 +0300

    Add core dependencies and configure custom Application class
    - Update target SDK to 37 and Kotlin to 2.4.0.
    - Add dependencies for Koin, Room, Retrofit, Ktor, Firebase, and Coil.
    - Implement custom `Application` class to initialize Koin.
    - Add `google-services.json` and configure Google Services/Crashlytics plugins.
    - Add JitPack repository and required app permissions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    id("kotlin-parcelize")
+    id("com.google.devtools.ksp")
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.google.firebase.crashlytics)
 }
 
 android {
     namespace = "com.example.hadar_food_delivery"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -14,7 +19,7 @@ android {
     defaultConfig {
         applicationId = "com.example.hadar_food_delivery"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
@@ -53,4 +58,54 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.firebase.messaging)
+    implementation(libs.firebase.crashlytics)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
+    implementation(libs.work.runtime.ktx)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.logging.interceptor)
+    implementation(libs.converter.gson)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    // coil
+    implementation(libs.coil.compose)
+
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.sdp.compose)
+    // exposed DropDown
+    implementation(libs.material3)
+    implementation(libs.accompanist.permissions)
+    implementation(libs.play.services.location)
+    // kotlin serialization
+    implementation(libs.kotlin.serialization.json)
+    // navigation compose
+    // biometric auth
+    implementation(libs.biometric)
+    // Encrypt SharedPreferences
+    implementation(libs.androidx.security.crypto)
+    // SQLCipher for encryption
+    implementation(libs.sqlcipher.android)
+    // Icons
+    implementation(libs.androidx.material.icons.core)
+    implementation(libs.androidx.material.icons.extended)
+    debugImplementation(libs.library)
+    releaseImplementation(libs.chucker.no.op)
+    // webSocket
+    implementation(libs.okhttp)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.serialization)
+    implementation(libs.ktor.client.websockets)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.content.negotiation)
+    // koin
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
 }
